ff07c0671852661373209ad3563fde0a6e3610b3
Dan Jacob and Jason Lazzuri <dan+jason@>
2017-05-24 14:36:57 -0700

    grab manufacturer name from the API

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,13 +1,17 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
 var bikeLocation = function(location) {
-  $('.showStolenLocation').text("All stolen bikes in " + location);
+  $('.showStolenLocation').append("All stolen bikes in " + "<li>" + location +"</li>");
+};
+var bikeManufacturer = function(bikeManufacturer) {
+  $('.showStolenMaker').append("Here are the manufacturer names:" + "<li>" + bikeManufacturer +"</li>");
 };
 
 $(document).ready(function() {
   var currentBikeObject = new Bike();
-  $('#location-button').click(function() {
+  $('#location-button').submit(function() {
+    event.preventDefault();
     var location = $('#location').val();
-    currentBikeObject.getBike(location, bikeLocation);
+    currentBikeObject.getBike(location, bikeLocation, bikeManufacturer);
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -2,9 +2,19 @@ function Bike(){
 
 }
 
-Bike.prototype.getBike = function(location, bikeLocation){
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=50&location='+location+'&distance=10&stolenness=proximity&access_token=response').then(function(response) {
-  console.log(response);
+Bike.prototype.getBike = function(location, bikeLocation, bikeManufacturer){
+  bikes = [];
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=50&location=' + location + '&distance=10&stolenness=proximity&access_token=response').then(function(response) {
+      var bikes = response.bikes;
+      bikes.forEach(function(bike) {
+        result = bikeLocation(bike.stolen_location);
+        result2 = bikeManufacturer(bike.manufacturer_name);
+
+      });
+
+      return result, result2;
+
+
   }).fail(function(error) {
     $('.showStolenLocation').text(error.responseJSON);
   });
